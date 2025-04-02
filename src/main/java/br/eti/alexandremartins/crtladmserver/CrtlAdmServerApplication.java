99f53f8438bae2c7446332a05c4338433b3d0561
Alexandre Martins <alexandre.martins.001@gmail.com>
2025-04-01 21:44:44 -0300

    first commit

---
+++ src/main/java/br/eti/alexandremartins/crtladmserver/CrtlAdmServerApplication.java
@@ -0,0 +1,15 @@
+package br.eti.alexandremartins.crtladmserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class CrtlAdmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrtlAdmServerApplication.class, args);
+    }
+
+}
